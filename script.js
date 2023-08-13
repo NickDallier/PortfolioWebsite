20fc82ddafebdeaa1fa1705d908e066fb95fb3e3
Nick Dallier <nickdallier@gmail.com>
2023-08-13 16:20:08 -0400

    Added responsive styling and Back to Top button

---
+++ script.js
@@ -0,0 +1,16 @@
+let topBtn = document.getElementById("topBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
